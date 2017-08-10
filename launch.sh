2b9ad8b7c0baae3a8628b3545733c0c0616d823b
Jamie Morton <jamietmorton@gmail.com>
2017-08-10 15:31:55 -0700

    Updating parameters

--- launch.sh
+++ launch.sh
@@ -1,11 +1,11 @@
 generate.py compositional_effect_size \
-    --max-alpha 10 --reps 30 --intervals 3 --n-species 20 \
+    --max-alpha 1000 --reps 1000 --intervals 11 --n-species 1000 \
     --output-dir pls_balances/results/effect_size_benchmarks
 
 generate.py compositional_variable_features \
-      --max-changing 5 --reps 30 --intervals 3 --n-species 10 \
+      --max-changing 500 --reps 1000 --intervals 11 --n-species 1000 \
       --fold-change 2 \
       --output-dir pls_balances/results/variable_features_benchmarks
 
-snakemake --cores 2 --configfile effect_size.yaml
-# snakemake --cores 2 --configfile variable_features.yaml
+snakemake --cores 8 --configfile effect_size.yaml
+snakemake --cores 8 --configfile variable_features.yaml
