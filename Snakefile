b4301d0ff79a1c1efb597da6e37802f5141bf60e
Jamie Morton <jamietmorton@gmail.com>
2017-08-09 18:18:09 -0700

    Finalizing snakemake

--- Snakefile
+++ Snakefile
@@ -2,15 +2,22 @@ import os
 import tempfile
 import numpy as np
 
+lam = 0.1
+category = 'effect_size'
+output_dir = '{output_dir}'
+n_contaminants = 30
+
 SAMPLES = np.arange(3).astype(np.str)
 TOOLS = ['ancom', 'pls_balances']
 
+# question : how can I get rid of test_data?
 
 rule all:
     input:
-        # expand("test_data/ancom.{sample}.results", sample=SAMPLES)
-        #"test_data/ancom.summary"
-        expand("test_data/{tool}.summary", tool=TOOLS)
+        # expand("{output_dir}/ancom.{sample}.results", sample=SAMPLES)
+        #"{output_dir}/ancom.summary"
+        # expand("{output_dir}/{tool}.summary", tool=TOOLS)
+        "test_data/confusion_matrix.summary"
 
 rule inject_noise:
     input:
@@ -21,8 +28,8 @@ rule inject_noise:
     run:
         shell("""
         generate.py noisify \
-            --lam 0.1 \
-            --n-contaminants 30 \
+            --lam {lam} \
+            --n-contaminants {n_contaminants} \
             --table-file {input.table} \
             --metadata-file {input.metadata} \
             --output-file {output}
@@ -46,7 +53,8 @@ rule run_tool:
 rule summarize:
     input:
         tables = expand("test_data/table.noisy.{sample}.biom", sample=SAMPLES),
-        results = expand("test_data/{tool}.{sample}.results", sample=SAMPLES, tool=TOOLS),
+        results = expand("test_data/{tool}.{sample}.results",
+                         tool=TOOLS, sample=SAMPLES),
         truths = expand("test_data/truth.{sample}.csv", sample=SAMPLES)
     output:
         "test_data/{tool}.summary"
@@ -55,8 +63,16 @@ rule summarize:
         compute_confusion_matrices(input.tables, input.results,
                                    input.truths, output[0])
 
-# rule aggregate_summaries:
-#     input:
-#         ancom='test_data/ancom.summary'
-#         pls='test_data/pls.summary'
+rule aggregate_summaries:
+    input:
+        summaries = expand("test_data/{tool}.summary", tool=TOOLS),
+        metadata = expand("test_data/metadata.{sample}.txt", sample=SAMPLES),
+        tables = expand("test_data/table.noisy.{sample}.biom", sample=SAMPLES),
+    output:
+        "test_data/confusion_matrix.summary"
+    run:
+        from pls_balances.src.evaluate import aggregate_summaries
+        aggregate_summaries(input.summaries, input.tables, input.metadata,
+                            category, output[0])
+
 
--- pls_balances/src/evaluate.py
+++ pls_balances/src/evaluate.py
@@ -1,9 +1,41 @@
 from biom import load_table
 import pandas as pd
+from os.path import basename, splitext
 
 
-def compute_confusion_matrices(table_files, result_files, truth_files, output_file):
-    stats = pd.DataFrame(index=table_files, columns=['TP', 'FP', 'FN', 'TN'])
+def compute_confusion_matrices(table_files, result_files,
+                               truth_files, output_file):
+    """ Computes confusion matrice over all runs for a
+    specified set of results.
+
+    Parameters
+    ----------
+    table_files : list of str
+        List of filepaths for biom tables.
+    result_files : list of str
+        List of filepaths for estimated differentially abundant features.
+    truth_files : list of str
+        List of filepaths for ground truth differentially abundant features.
+    output_file : str
+        File path for confusion matrix summary.
+
+    Note
+    ----
+    This assumes that the tables all have the same basename.
+    """
+    # only use the result files that match with the output_file
+    out_suf = splitext(basename(output_file))[0]
+    result_files = list(filter(lambda x: out_suf in x, result_files))
+
+    index_names = list(map(lambda x: splitext(basename(x))[0], table_files))
+    suf = splitext(basename(result_files[0]))[0]
+    col_names = ['%s_TP' % suf,
+                 '%s_FP' % suf,
+                 '%s_FN' % suf,
+                 '%s_TN' % suf]
+    TP, FP, FN, TN = 0, 1, 2, 3
+
+    stats = pd.DataFrame(columns=col_names, index=table_files)
     for tab_file, r_file, t_file in zip(table_files,
                                         result_files,
                                         truth_files):
@@ -13,10 +45,47 @@ def compute_confusion_matrices(table_files, result_files, truth_files, output_fi
         ids = set(load_table(tab_file).ids(axis='observation'))
 
         x = pd.Series(
-            {'TP': len(hits & truth),
-             'FP': len(hits - truth),
-             'FN': len(truth - hits),
-             'TN': len((ids-hits) & (ids-truth))
+            {col_names[TP]: len(hits & truth),
+             col_names[FP]: len(hits - truth),
+             col_names[FN]: len(truth - hits),
+             col_names[TN]: len((ids-hits) & (ids-truth))
             })
         stats.loc[tab_file] = x
     stats.to_csv(output_file, sep='\t')
+
+
+def aggregate_summaries(confusion_matrix_files, table_files, metadata_files,
+                        axis, output_file):
+    """ Aggregates summary files together, along with the variable of interest.
+
+    Parameters
+    ----------
+    confusion_matrix_files : list of str
+        List of filepaths for summaries.
+    table_files : list of str
+        List of filepaths for biom tables.
+    metadata_files : list of str
+        List of filepaths for metadata files.
+    axis : str
+        Category of differentiation.
+    output_file : str
+        Output path for aggregated summaries.
+
+    Note
+    ----
+    This assumes that table_files and metadata_files
+    are in the same matching order.
+
+    table.xxx.biom
+    metadata.xxx.txt
+    """
+    mats = [pd.read_table(f, index_col=0) for f in confusion_matrix_files]
+    merged_stats = pd.concat(mats, axis=1)
+
+    # first
+    index_names = table_files
+    # aggregate stats in all metadata_files. For now, just take the mean
+    x = [pd.read_table(f, index_col=0)[axis].mean() for f in metadata_files]
+    cats = pd.DataFrame(x, index=index_names, columns=[axis])
+    merged_stats = pd.merge(merged_stats, cats, left_index=True, right_index=True)
+    merged_stats.to_csv(output_file, sep='\t')
